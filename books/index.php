aab64cdf8efea35df5ef90cbe09b048f1b4590fc
manoj24092003 <146913136+manoj24092003@users.noreply.github.com>
2026-03-05 01:35:59 +0530

    Add files via upload

---
+++ books/index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manoj Writes</title>
+    <meta name="title" content="ManojWrites">
+    <meta name="theme-color" content="#000000">
+
+
+    <!--font awesome-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+    <!--font awesome-->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+
+
+
+  
+  <link rel="stylesheet" href="../style.css?v=7">
+
+</head>
+
+
+           
+ 
+        <header class="header">
+    <div class="logo" onclick="location.href='/index.php'"><i class="bi bi-tv-fill tv-logo"></i></div>
+
+    <!-- SEARCH BAR -->
+    <div class="search-box" id="search-box">
+        <i class="bi bi-search search-icon"></i>
+        <input type="search" id="searchInput" placeholder="Search..." autocomplete="off">
+    </div>
+
+    <div id="searchResults"></div>
+    
+    <div class="rcontainer">
+<i class="bi bi-search search-icon rsearch"></i>
+
+
+    <!-- Desktop navigation -->
+    <nav class="nav-desktop" aria-label="Primary">
+        <ul>
+            <li>
+                <a href="../index.php">
+                    <i class="bi bi-house-fill"></i> HOME
+                </a>
+            </li>
+
+            <li>
+                <a href="./index.php">
+                    <i class="bi bi-book-half"></i> BOOKS
+                </a>
+            </li>
+
+            <li>
+                <a href="../poems/index.php">
+                    <i class="bi bi-vector-pen"></i> POEMS
+                </a>
+            </li>
+
+            <li>
+                <a href="../login/index.php">
+                    <i class="bi bi-person-fill"></i> ADMIN
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+  
+
+        <!-- Burger button for mobile -->
+        <div>
+            <button id="menuBtn"
+                class="menu-btn"
+                aria-label="Open menu"
+                aria-expanded="false"
+                aria-controls="mobileMenu">
+                <span class="bar"></span>
+            </button>
+        </div>
+    </div>
+</header>
+
+<!-- Mobile menu -->
+<nav id="mobileMenu"
+    class="mobile-panel"
+    aria-hidden="true"
+    aria-label="Mobile primary">
+             <a href="../index.php"><i class="bi bi-house-fill"></i> HOME</a>
+  <a href="./index.php"><i class="bi bi-book-half"></i> BOOKS</a>
+  <a href="../poems/index.php"><i class="bi bi-vector-pen"></i> POEMS</a>
+  <a href="../login/index.php"><i class="bi bi-person-fill"></i> ADMIN</a>
+</nav>
+            
+            
+            
+            
+            <!------------------mainsite section----------------->
+<main class="main">
+
+
+<!---------------------- hero section -------------------------->
+
+
+<section class="hero" style="padding:1rem;height:100vh;">
+    
+    <h4>coming soon...</h4>
+      
+     </section>
+
+        
+        
+        
+        
+<!--------- hero section end ------------------->
+
+<div>
+
+  </div>
+
+<div class="footer">
+    <div class="copy">
+   <p>&copy2025.</p>
+    </div>
+
+    <div class="social">
+       
+</div>
+ 
+</div>
+
+
+</main>
+
+
+
+
+    <!-----------java script------------------->
+<!--<script src="/home/jquery.3.7.1.min.js"></script>-->
+    <script src="./book.js?v=2"></script>
+   
+
+
+
+    <!-----------java script------------------->
+
+
+</html>
